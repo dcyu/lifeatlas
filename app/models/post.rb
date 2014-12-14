686ed127043364c7bcd3b29af760d48113d29b62
Daniel Yu <dcyu93@gmail.com>
2014-12-14 00:24:16 -0600

    additional aesthetics for rating system

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 class Post < ActiveRecord::Base
   belongs_to :destination
   belongs_to :restaurant
+
+  def rating_hint
+    if rating == 1
+      "Utterly Disappointing"
+    elsif rating == 2
+      "Mediocre"
+    elsif rating == 3
+      "Decent"
+    elsif rating == 4
+      "Enjoyable"
+    elsif rating == 5
+      "Gamechanger"
+    end
+  end
 end
