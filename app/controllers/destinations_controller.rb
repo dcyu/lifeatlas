6f4ac0d2dae51a1ed560d08b040c4fad64003d43
Daniel Yu <daniel@sokowatch.com>
2025-07-29 15:13:59 -0600

    redirect new destination

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -61,7 +61,7 @@ class DestinationsController < ApplicationController
 
     respond_to do |format|
       if @destination.save
-        format.html { redirect_to destinations_path, notice: 'Destination was successfully created.' }
+        format.html { redirect_to @destination, notice: 'Destination was successfully created.' }
         format.json { render :show, status: :created, location: @destination }
       else
         format.html { render :new }
